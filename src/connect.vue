8f68fca62a35f33c840d9dd0e2acd692b607f8b8
enochtangg <enochtangg@gmail.com>
2018-02-17 13:42:43 -0500

    dist folder

--- src/connect.vue
+++ src/connect.vue
@@ -1,5 +1,14 @@
 <template>
-
+    <div>
+        <md-content class="md-primary" id="header">
+            <div class="header-container">
+                <h1>Connect with Me</h1>
+                <p class="subheading"></p>
+            </div>
+        </md-content>
+        <div class="md-layout" id="container">
+        </div>
+    </div>
 </template>
 
 <script>
@@ -9,5 +18,28 @@
 </script>
 
 <style scoped>
+    .md-layout {
+        width: 90%;
+        margin: auto;
+    }
+
+    h1 {
+        font-size: 250%;
+    }
+
+    .header-container {
+        padding-left: 100px;
+        padding-top: 30px;
+        height: 200px;
+    }
+
+    #header {
+        width: 100%;
+    }
+
+    .subheading {
+        font-size: 125%;
+        color: #C1BFBF;
+    }
 
 </style>
--- src/photography.vue
+++ src/photography.vue
@@ -1,199 +1,203 @@
 <template>
-    <div class="md-layout" id="container">
+    <div>
         <md-content class="md-primary" id="header">
             <div class="header-container">
                 <h1>Photography Collection</h1>
                 <p class="subheading"></p>
             </div>
         </md-content>
+        <div class="md-layout" id="container">
+            <md-card md-with-hover class="image_tile">
+                <md-card-media-cover>
+                    <md-card-media md-ratio="16:9">
+                        <a class="lightbox" rel="gallery" href="/pictures/niagarafalls.jpg"
+                           data-caption="Niagara Falls">
+                            <img src="/pictures/niagarafalls.jpg"/>
+                        </a>
+                    </md-card-media>
+                </md-card-media-cover>
+            </md-card>
 
-        <md-card md-with-hover class="image_tile">
-            <md-card-media-cover>
-                <md-card-media md-ratio="16:9">
-                    <a class="lightbox" rel="gallery" href="/pictures/niagarafalls.jpg" data-caption="Niagara Falls">
-                        <img src="/pictures/niagarafalls.jpg"/>
-                    </a>
-                </md-card-media>
-            </md-card-media-cover>
-        </md-card>
+            <md-card md-with-hover class="image_tile">
+                <md-card-media-cover>
+                    <md-card-media md-ratio="16:9">
+                        <a class="lightbox" rel="gallery" href="/pictures/sunset.jpg" data-caption="Chasing the Sun">
+                            <img src="/pictures/sunset.jpg">
+                        </a>
+                    </md-card-media>
+                </md-card-media-cover>
+            </md-card>
+            <md-card md-with-hover class="image_tile">
+                <md-card-media-cover>
+                    <md-card-media md-ratio="16:9">
+                        <a class="lightbox" rel="gallery" href="/pictures/lightfest.jpg" data-caption="Origami">
+                            <img src="/pictures/lightfest.jpg">
+                        </a>
+                    </md-card-media>
+                </md-card-media-cover>
+            </md-card>
+            <md-card md-with-hover class="image_tile">
+                <md-card-media-cover>
+                    <md-card-media md-ratio="16:9">
+                        <a class="lightbox" rel="gallery" href="/pictures/disk.jpg" data-caption="For the Record">
+                            <img src="/pictures/disk.jpg">
+                        </a>
+                    </md-card-media>
+                </md-card-media-cover>
+            </md-card>
+            <md-card md-with-hover class="image_tile">
+                <md-card-media-cover>
+                    <md-card-media md-ratio="16:9">
+                        <a class="lightbox" rel="gallery" href="/pictures/cntower.jpg" data-caption="Sunset in Toronto">
+                            <img src="/pictures/cntower.jpg">
+                        </a>
+                    </md-card-media>
+                </md-card-media-cover>
+            </md-card>
 
-        <md-card md-with-hover class="image_tile">
-            <md-card-media-cover>
-                <md-card-media md-ratio="16:9">
-                    <a class="lightbox" rel="gallery" href="/pictures/sunset.jpg" data-caption="Chasing the Sun">
-                        <img src="/pictures/sunset.jpg">
-                    </a>
-                </md-card-media>
-            </md-card-media-cover>
-        </md-card>
-        <md-card md-with-hover class="image_tile">
-            <md-card-media-cover>
-                <md-card-media md-ratio="16:9">
-                    <a class="lightbox" rel="gallery" href="/pictures/lightfest.jpg" data-caption="Origami">
-                        <img src="/pictures/lightfest.jpg">
-                    </a>
-                </md-card-media>
-            </md-card-media-cover>
-        </md-card>
-        <md-card md-with-hover class="image_tile">
-            <md-card-media-cover>
-                <md-card-media md-ratio="16:9">
-                    <a class="lightbox" rel="gallery" href="/pictures/disk.jpg" data-caption="For the Record">
-                        <img src="/pictures/disk.jpg">
-                    </a>
-                </md-card-media>
-            </md-card-media-cover>
-        </md-card>
-        <md-card md-with-hover class="image_tile">
-            <md-card-media-cover>
-                <md-card-media md-ratio="16:9">
-                    <a class="lightbox" rel="gallery" href="/pictures/cntower.jpg" data-caption="Sunset in Toronto">
-                        <img src="/pictures/cntower.jpg">
-                    </a>
-                </md-card-media>
-            </md-card-media-cover>
-        </md-card>
+            <md-card md-with-hover class="image_tile">
+                <md-card-media-cover>
+                    <md-card-media md-ratio="16:9">
+                        <a class="lightbox" rel="gallery" href="/pictures/wedding.jpg"
+                           data-caption="Wedding at Four Season Hotel">
+                            <img src="/pictures/wedding.jpg">
+                        </a>
+                    </md-card-media>
+                </md-card-media-cover>
+            </md-card>
+            <md-card md-with-hover class="image_tile">
+                <md-card-media-cover>
+                    <md-card-media md-ratio="16:9">
+                        <a class="lightbox" rel="gallery" href="/pictures/kensington.jpg"
+                           data-caption="Hustlin' and Bustlin'">
+                            <img src="/pictures/kensington.jpg">
+                        </a>
+                    </md-card-media>
+                </md-card-media-cover>
+            </md-card>
+            <md-card md-with-hover class="image_tile">
+                <md-card-media-cover>
+                    <md-card-media md-ratio="16:9">
+                        <a class="lightbox" rel="gallery" href="/pictures/winter.jpg" data-caption="Winter is Coming">
+                            <img src="/pictures/winter.jpg">
+                        </a>
+                    </md-card-media>
+                </md-card-media-cover>
+            </md-card>
+            <md-card md-with-hover class="image_tile">
+                <md-card-media-cover>
+                    <md-card-media md-ratio="16:9">
+                        <a class="lightbox" rel="gallery" href="/pictures/godwin.jpg" data-caption="Riverdale Park">
+                            <img src="/pictures/godwin.jpg">
+                        </a>
+                    </md-card-media>
+                </md-card-media-cover>
+            </md-card>
+            <md-card md-with-hover class="image_tile">
+                <md-card-media-cover>
+                    <md-card-media md-ratio="16:9">
+                        <a class="lightbox" rel="gallery" href="/pictures/youngdundas.jpg" data-caption="Intersection">
+                            <img src="/pictures/youngdundas.jpg">
+                        </a>
+                    </md-card-media>
+                </md-card-media-cover>
+            </md-card>
 
-        <md-card md-with-hover class="image_tile">
-            <md-card-media-cover>
-                <md-card-media md-ratio="16:9">
-                    <a class="lightbox" rel="gallery" href="/pictures/wedding.jpg"
-                       data-caption="Wedding at Four Season Hotel">
-                        <img src="/pictures/wedding.jpg">
-                    </a>
-                </md-card-media>
-            </md-card-media-cover>
-        </md-card>
-        <md-card md-with-hover class="image_tile">
-            <md-card-media-cover>
-                <md-card-media md-ratio="16:9">
-                    <a class="lightbox" rel="gallery" href="/pictures/kensington.jpg"
-                       data-caption="Hustlin' and Bustlin'">
-                        <img src="/pictures/kensington.jpg">
-                    </a>
-                </md-card-media>
-            </md-card-media-cover>
-        </md-card>
-        <md-card md-with-hover class="image_tile">
-            <md-card-media-cover>
-                <md-card-media md-ratio="16:9">
-                    <a class="lightbox" rel="gallery" href="/pictures/winter.jpg" data-caption="Winter is Coming">
-                        <img src="/pictures/winter.jpg">
-                    </a>
-                </md-card-media>
-            </md-card-media-cover>
-        </md-card>
-        <md-card md-with-hover class="image_tile">
-            <md-card-media-cover>
-                <md-card-media md-ratio="16:9">
-                    <a class="lightbox" rel="gallery" href="/pictures/godwin.jpg" data-caption="Riverdale Park">
-                        <img src="/pictures/godwin.jpg">
-                    </a>
-                </md-card-media>
-            </md-card-media-cover>
-        </md-card>
-        <md-card md-with-hover class="image_tile">
-            <md-card-media-cover>
-                <md-card-media md-ratio="16:9">
-                    <a class="lightbox" rel="gallery" href="/pictures/youngdundas.jpg" data-caption="Intersection">
-                        <img src="/pictures/youngdundas.jpg">
-                    </a>
-                </md-card-media>
-            </md-card-media-cover>
-        </md-card>
+            <md-card md-with-hover class="image_tile">
+                <md-card-media-cover>
+                    <md-card-media md-ratio="16:9">
+                        <a class="lightbox" rel="gallery" href="/pictures/shoes.jpg" data-caption="One Square Foot">
+                            <img src="/pictures/shoes.jpg">
+                        </a>
+                    </md-card-media>
+                </md-card-media-cover>
+            </md-card>
+            <md-card md-with-hover class="image_tile">
+                <md-card-media-cover>
+                    <md-card-media md-ratio="16:9">
+                        <a class="lightbox" rel="gallery" href="/pictures/street.jpg" data-caption="Redlight">
+                            <img src="/pictures/street.jpg">
+                        </a>
+                    </md-card-media>
+                </md-card-media-cover>
+            </md-card>
+            <md-card md-with-hover class="image_tile">
+                <md-card-media-cover>
+                    <md-card-media md-ratio="16:9">
+                        <a class="lightbox" rel="gallery" href="/pictures/jumpman.jpg" data-caption="Jumpman">
+                            <img src="/pictures/jumpman.jpg">
+                        </a>
+                    </md-card-media>
+                </md-card-media-cover>
+            </md-card>
+            <md-card md-with-hover class="image_tile">
+                <md-card-media-cover>
+                    <md-card-media md-ratio="16:9">
+                        <a class="lightbox" rel="gallery" href="/pictures/tree.jpg"
+                           data-caption="Brother's Neighbourhood">
+                            <img src="/pictures/tree.jpg">
+                        </a>
+                    </md-card-media>
+                </md-card-media-cover>
+            </md-card>
+            <md-card md-with-hover class="image_tile">
+                <md-card-media-cover>
+                    <md-card-media md-ratio="16:9">
+                        <a class="lightbox" rel="gallery" href="/pictures/sixupsidedown.jpg"
+                           data-caption="Turn the 6 upside down, it's a 9 now">
+                            <img src="/pictures/sixupsidedown.jpg">
+                        </a>
+                    </md-card-media>
+                </md-card-media-cover>
+            </md-card>
+            <md-card md-with-hover class="image_tile">
+                <md-card-media-cover>
+                    <md-card-media md-ratio="16:9">
+                        <a class="lightbox" rel="gallery" href="/pictures/fallstreet.jpg" data-caption="Autumn">
+                            <img src="/pictures/fallstreet.jpg">
+                        </a>
+                    </md-card-media>
+                </md-card-media-cover>
+            </md-card>
 
-        <md-card md-with-hover class="image_tile">
-            <md-card-media-cover>
-                <md-card-media md-ratio="16:9">
-                    <a class="lightbox" rel="gallery" href="/pictures/shoes.jpg" data-caption="One Square Foot">
-                        <img src="/pictures/shoes.jpg">
-                    </a>
-                </md-card-media>
-            </md-card-media-cover>
-        </md-card>
-        <md-card md-with-hover class="image_tile">
-            <md-card-media-cover>
-                <md-card-media md-ratio="16:9">
-                    <a class="lightbox" rel="gallery" href="/pictures/street.jpg" data-caption="Redlight">
-                        <img src="/pictures/street.jpg">
-                    </a>
-                </md-card-media>
-            </md-card-media-cover>
-        </md-card>
-        <md-card md-with-hover class="image_tile">
-            <md-card-media-cover>
-                <md-card-media md-ratio="16:9">
-                    <a class="lightbox" rel="gallery" href="/pictures/jumpman.jpg" data-caption="Jumpman">
-                        <img src="/pictures/jumpman.jpg">
-                    </a>
-                </md-card-media>
-            </md-card-media-cover>
-        </md-card>
-        <md-card md-with-hover class="image_tile">
-            <md-card-media-cover>
-                <md-card-media md-ratio="16:9">
-                    <a class="lightbox" rel="gallery" href="/pictures/tree.jpg" data-caption="Brother's Neighbourhood">
-                        <img src="/pictures/tree.jpg">
-                    </a>
-                </md-card-media>
-            </md-card-media-cover>
-        </md-card>
-        <md-card md-with-hover class="image_tile">
-            <md-card-media-cover>
-                <md-card-media md-ratio="16:9">
-                    <a class="lightbox" rel="gallery" href="/pictures/sixupsidedown.jpg"
-                       data-caption="Turn the 6 upside down, it's a 9 now">
-                        <img src="/pictures/sixupsidedown.jpg">
-                    </a>
-                </md-card-media>
-            </md-card-media-cover>
-        </md-card>
-        <md-card md-with-hover class="image_tile">
-            <md-card-media-cover>
-                <md-card-media md-ratio="16:9">
-                    <a class="lightbox" rel="gallery" href="/pictures/fallstreet.jpg" data-caption="Autumn">
-                        <img src="/pictures/fallstreet.jpg">
-                    </a>
-                </md-card-media>
-            </md-card-media-cover>
-        </md-card>
-
-        <md-card md-with-hover class="image_tile">
-            <md-card-media-cover>
-                <md-card-media md-ratio="16:9">
-                    <a class="lightbox" rel="gallery" href="/pictures/worship.jpg" data-caption="Praise and Prayer">
-                        <img src="/pictures/worship.jpg">
-                    </a>
-                </md-card-media>
-            </md-card-media-cover>
-        </md-card>
-        <md-card md-with-hover class="image_tile">
-            <md-card-media-cover>
-                <md-card-media md-ratio="16:9">
-                    <a class="lightbox" rel="gallery" href="/pictures/pirateblack.jpg" data-caption="Top of the City">
-                        <img src="/pictures/pirateblack.jpg">
-                    </a>
-                </md-card-media>
-            </md-card-media-cover>
-        </md-card>
-        <md-card md-with-hover class="image_tile">
-            <md-card-media-cover>
-                <md-card-media md-ratio="16:9">
-                    <a class="lightbox" rel="gallery" href="/pictures/haveaseat.jpg" data-caption="Have a Seat">
-                        <img src="/pictures/haveaseat.jpg">
-                    </a>
-                </md-card-media>
-            </md-card-media-cover>
-        </md-card>
-        <md-card md-with-hover class="image_tile">
-            <md-card-media-cover>
-                <md-card-media md-ratio="16:9">
-                    <a class="lightbox" rel="gallery" href="/pictures/firewire.jpg" data-caption="Fire Wire">
-                        <img src="/pictures/firewire.jpg">
-                    </a>
-                </md-card-media>
-            </md-card-media-cover>
-        </md-card>
+            <md-card md-with-hover class="image_tile">
+                <md-card-media-cover>
+                    <md-card-media md-ratio="16:9">
+                        <a class="lightbox" rel="gallery" href="/pictures/worship.jpg" data-caption="Praise and Prayer">
+                            <img src="/pictures/worship.jpg">
+                        </a>
+                    </md-card-media>
+                </md-card-media-cover>
+            </md-card>
+            <md-card md-with-hover class="image_tile">
+                <md-card-media-cover>
+                    <md-card-media md-ratio="16:9">
+                        <a class="lightbox" rel="gallery" href="/pictures/pirateblack.jpg"
+                           data-caption="Top of the City">
+                            <img src="/pictures/pirateblack.jpg">
+                        </a>
+                    </md-card-media>
+                </md-card-media-cover>
+            </md-card>
+            <md-card md-with-hover class="image_tile">
+                <md-card-media-cover>
+                    <md-card-media md-ratio="16:9">
+                        <a class="lightbox" rel="gallery" href="/pictures/haveaseat.jpg" data-caption="Have a Seat">
+                            <img src="/pictures/haveaseat.jpg">
+                        </a>
+                    </md-card-media>
+                </md-card-media-cover>
+            </md-card>
+            <md-card md-with-hover class="image_tile">
+                <md-card-media-cover>
+                    <md-card-media md-ratio="16:9">
+                        <a class="lightbox" rel="gallery" href="/pictures/firewire.jpg" data-caption="Fire Wire">
+                            <img src="/pictures/firewire.jpg">
+                        </a>
+                    </md-card-media>
+                </md-card-media-cover>
+            </md-card>
+        </div>
     </div>
 </template>
 
@@ -220,7 +224,7 @@
         width: 24.4%;
         margin: 0.3%;
         display: inline-block;
-        top: 100px;
+        top: 10px;
     }
 
     .md-layout {
